fc41e8e5e8144071b86591c7415dccba92ad2d3b
zerocho <zerohch0@gmail.com>
2019-04-23 00:40:01 +0900

    Update: 숫자야구 class examples

---
+++ 3.숫자야구/NumberBaseballClass.jsx
@@ -0,0 +1,97 @@
+import Try from './Try';
+
+function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+  const candidate = [1,2,3,4,5,6,7,8,9];
+  const array = [];
+  for (let i = 0; i < 4; i += 1) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+}
+
+class NumberBaseball extends Component {
+  state = {
+    result: '',
+    value: '',
+    answer: getNumbers(), // ex: [1,3,5,7]
+    tries: [], // push 쓰면 안 돼요
+  };
+
+  onSubmitForm = (e) => {
+    const { value, tries, answer } = this.state;
+    e.preventDefault();
+    if (value === answer.join('')) {
+      this.setState((prevState) => {
+        return {
+          result: '홈런!',
+          tries: [...prevState.tries, { try: value, result: '홈런!' }],
+        }
+      });
+      alert('게임을 다시 시작합니다!');
+      this.setState({
+        value: '',
+        answer: getNumbers(),
+        tries: [],
+      });
+    } else { // 답 틀렸으면
+      const answerArray = value.split('').map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (tries.length >= 9) { // 10번 이상 틀렸을 때
+        this.setState({
+          result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
+        });
+        alert('게임을 다시 시작합니다!');
+        this.setState({
+          value: '',
+          answer: getNumbers(),
+          tries: [],
+        });
+      } else {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === answer[i]) {
+            strike += 1;
+          } else if (answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        this.setState((prevState) => {
+          return {
+            tries: [...prevState.tries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
+            value: '',
+          };
+        });
+      }
+    }
+  };
+
+  onChangeInput = (e) => {
+    console.log(this.state.answer);
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  render() {
+    const { result, value, tries } = this.state;
+    return (
+      <>
+        <h1>{result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input maxLength={4} value={value} onChange={this.onChangeInput} />
+        </form>
+        <div>시도: {tries.length}</div>
+        <ul>
+          {tries.map((v, i) => {
+            return (
+              <Try key={`${i + 1}차 시도 :`} tryInfo={v} />
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default NumberBaseball; // import NumberBaseball;
---
+++ 3.숫자야구/TryClass.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Try extends Component {
+  render() {
+    const { tryInfo } = this.props;
+    return (
+      <li>
+        <div>{tryInfo.try}</div>
+        <div>{tryInfo.result}</div>
+      </li>
+    );
+  }
+}
+
+export default Try;
