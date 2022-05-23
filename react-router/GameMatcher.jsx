5e5baba5f37cef028fd39be9f7bee43240d3ec4f
Hyunyoung Cho <zerohch0@gmail.com>
2022-05-23 10:43:40 +0900

    Update GameMatcher.jsx

--- react-router/GameMatcher.jsx
+++ react-router/GameMatcher.jsx
@@ -4,27 +4,25 @@ import RSP from '../5.가위바위보/RSPClass';
 import Lotto from '../6.로또/LottoClass';
 import {useLocation, useNavigate, Routes, Route} from 'react-router';
 
-class GameMatcher extends Component {
-  render() {
-    const location = useLocation();
-    const navigate = useNavigate();
-    let urlSearchParams = new URLSearchParams(this.props.location.search.slice(1));
-    console.log(urlSearchParams.get('hello'));
-    console.log(urlSearchParams.get('page'));
-    return (
-      <Routes>
-        <Route path="number-baseball" element={<NumberBaseball />} />
-        <Route path="rock-scissors-paper" element={<RSP />} />
-        <Route path="lotto-generator" element={<Lotto />} />
-        <Route
-          path="*"
-          element={<div>
-            일치하는 게임이 없습니다.
-          </div>}
-        />
-      </Routes>
-    );
-  }
+const GameMatcher = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  let urlSearchParams = new URLSearchParams(location.search.slice(1));
+  console.log(urlSearchParams.get('hello'));
+  console.log(urlSearchParams.get('page'));
+  return (
+    <Routes>
+      <Route path="number-baseball" element={<NumberBaseball />} />
+      <Route path="rock-scissors-paper" element={<RSP />} />
+      <Route path="lotto-generator" element={<Lotto />} />
+      <Route
+        path="*"
+        element={<div>
+          일치하는 게임이 없습니다.
+        </div>}
+      />
+    </Routes>
+  );
 }
 
 export default GameMatcher;
